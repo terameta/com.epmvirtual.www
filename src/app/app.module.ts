35997b09d48caf0a863480afd1d34aa447e574cd
Ali Dikici <aliriza.dikici@gmail.com>
2018-06-11 23:41:47 +0400

    More on the front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { systemSettings } from './app.settings';
+import { AngularFireModule } from 'angularfire2';
+import { AppRoutingModule } from './app.routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(systemSettings.firebase),
+		AppRoutingModule,
+		SharedModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.settings.ts
@@ -0,0 +1,11 @@
+export const systemSettings = {
+	firebase: {
+		apiKey: 'AIzaSyDohq4yWueCGncjlFJu9ScD1nMHQJ_LeZw',
+		authDomain: 'epmvirtual-d8014.firebaseapp.com',
+		databaseURL: 'https://epmvirtual-d8014.firebaseio.com',
+		projectId: 'epmvirtual-d8014',
+		storageBucket: 'epmvirtual-d8014.appspot.com',
+		messagingSenderId: '830847148266',
+		timestampsInSnapshots: true
+	}
+};
