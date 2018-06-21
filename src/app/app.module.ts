bb1934e8354c6c1e1c66b1976a0fb6e48d67452f
Ali Dikici <aliriza.dikici@gmail.com>
2018-06-21 20:23:09 +0400

    Designing the front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { systemSettings } from './app.settings';
 import { AngularFireModule } from 'angularfire2';
-import { AppRoutingModule } from './app.routing.module';
 import { SharedModule } from './shared/shared.module';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+	{ path: '', component: FrontPageComponent }
+];
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		FrontPageComponent,
 	],
 	imports: [
 		BrowserModule,
 		AngularFireModule.initializeApp(systemSettings.firebase),
-		AppRoutingModule,
+		RouterModule.forRoot(routes),
 		SharedModule
 	],
 	providers: [],
