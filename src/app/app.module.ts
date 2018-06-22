6d67a43fbb65662f5f3ba04d73197621beac0e8c
Ali Dikici <aliriza.dikici@gmail.com>
2018-06-23 00:09:52 +0400

    TOS updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { AngularFireModule } from 'angularfire2';
 import { SharedModule } from './shared/shared.module';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { Routes, RouterModule } from '@angular/router';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 const routes: Routes = [
-	{ path: '', component: FrontPageComponent }
+	{ path: '', component: FrontPageComponent },
+	{ path: 'terms-of-service', component: TermsOfServiceComponent }
 ];
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		FrontPageComponent,
+		TermsOfServiceComponent,
 	],
 	imports: [
 		BrowserModule,
