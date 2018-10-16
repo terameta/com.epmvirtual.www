8a590602f599a0f6cde388441b0f02025160d51e
Ali Dikici <aliriza.dikici@gmail.com>
2018-10-16 23:56:15 +0400

    Working on node terminal

--- src/app/admin/admin-nodes/admin-node-detail/admin-node-detail.component.ts
+++ src/app/admin/admin-nodes/admin-node-detail/admin-node-detail.component.ts
@@ -1,19 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { SharedService } from 'src/app/shared/shared.service';
+import { filter, map } from 'rxjs/operators';
+import { Node, defaultNode } from 'src/app/models/node.models';
+import { subsCreate, subsDispose } from 'src/utilities/ngUtilities';
+import { ItemType } from 'src/app/models/generic.models';
+import { Terminal } from 'xterm';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase/app';
 
 @Component( {
 	selector: 'app-admin-node-detail',
 	templateUrl: './admin-node-detail.component.html',
 	styleUrls: [ './admin-node-detail.component.scss' ]
 } )
-export class AdminNodeDetailComponent implements OnInit {
+export class AdminNodeDetailComponent implements OnInit, OnDestroy, AfterViewInit {
+	public item: Node = defaultNode();
+	public term: Terminal;
+	@ViewChild( 'terminal' ) terminal: ElementRef;
+
+	private subs = subsCreate();
 
 	constructor(
-		private ss: SharedService
+		private ss: SharedService,
+		private db: AngularFirestore
 	) { }
 
-	ngOnInit() {
-		this.ss.cItem$.subscribe( console.log );
+	ngAfterViewInit() {
+		this.term = new Terminal( { cursorBlink: true, scrollback: 60 } );
+		this.term.on( 'key', ( key, ev ) => {
+			console.log( key.charCodeAt( 0 ) );
+			if ( key.charCodeAt( 0 ) === 13 ) this.term.write( '\n' );
+			this.term.write( key );
+			this.db.doc( '/nodes/' + this.item.id ).update( {
+				keypressed: firestore.FieldValue.arrayUnion( { key, date: new Date() } )
+			} );
+		} );
+		this.term.open( this.terminal.nativeElement );
+
 	}
 
+	ngOnInit() {
+		this.subs.push( this.ss.cItem$.pipe(
+			filter( i => !!i.id ),
+			map( i => ( { ...i, type: ItemType.node } as Node ) )
+		).subscribe( i => {
+			this.item = i;
+			this.term.focus();
+		} ) );
+	}
+
+	ngOnDestroy() { subsDispose( this.subs ); }
 }
--- src/app/models/node.models.ts
+++ src/app/models/node.models.ts
@@ -1,15 +1,17 @@
-import { Item } from './generic.models';
+import { Item, ItemType } from './generic.models';
 
 export interface Node extends NodeCandidate {
 	name: string
 }
+
+export const defaultNode = (): Node => ( { id: '', name: '', type: ItemType.node } as Node );
 
 export interface NodeCandidateObject {
 	id: string,
 	items: NodeCandidate[]
 }
 
-export interface NodeCandidate extends Item {
+export interface NodeCandidate extends Partial<Item> {
 	id: string,
 	hostname: string,
 	ostype: string,
