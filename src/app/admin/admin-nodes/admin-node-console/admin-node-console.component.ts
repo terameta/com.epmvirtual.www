8046754cd302578043c300854b58dad325c44a11
Ali Dikici <aliriza.dikici@gmail.com>
2018-10-22 23:49:21 +0400

    Still working on console

--- src/app/admin/admin-nodes/admin-node-console/admin-node-console.component.ts
+++ src/app/admin/admin-nodes/admin-node-console/admin-node-console.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, HostListener, AfterContentInit } from '@angular/core';
 import { Terminal } from 'xterm';
+import { fit } from 'xterm/lib/addons/fit/fit';
 import { subsCreate, subsDispose } from 'src/utilities/ngUtilities';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { SharedService } from 'src/app/shared/shared.service';
@@ -14,13 +15,27 @@ import { firestore } from 'firebase/app';
 	templateUrl: './admin-node-console.component.html',
 	styleUrls: [ './admin-node-console.component.scss' ]
 } )
-export class AdminNodeConsoleComponent implements OnInit, AfterViewInit, OnDestroy {
+export class AdminNodeConsoleComponent implements OnInit, AfterContentInit, OnDestroy {
+	public screenHeight = 300;
+	public screenWidth = 100;
+	private afterIneted = false;
 	@ViewChild( 'terminal' ) terminal: ElementRef;
+
 	public node: Node = defaultNode();
 	public term: Terminal;
 
 	private subs = subsCreate();
 
+	@HostListener( 'window:resize', [ '$event' ] ) onResize( event?) {
+		if ( this.afterIneted ) {
+			this.screenHeight = window.innerHeight;
+			this.screenWidth = window.innerWidth;
+			fit( this.term );
+			console.log( 'Host listener window resized' );
+		} else {
+			console.log( 'Host listener, is called but doing nothing, afterinited is false' );
+		}
+	}
 	constructor(
 		private ss: SharedService,
 		private db: AngularFirestore
@@ -45,18 +60,34 @@ export class AdminNodeConsoleComponent implements OnInit, AfterViewInit, OnDestr
 				}
 			}
 		} ) );
+		setTimeout( () => {
+			// this.term.resize( 10, 20 );
+			fit( this.term );
+		}, 4000 );
+		setInterval( () => {
+			console.log( this.term.rows, this.term.cols, this.term.textarea.cols, this.term.textarea.rows );
+		}, 1000 );
 	}
 
 	ngOnDestroy() { subsDispose( this.subs ); }
 
-	ngAfterViewInit() {
-		this.term = new Terminal( { cursorBlink: true, scrollback: 60 } );
+	ngAfterContentInit() {
+		this.afterIneted = true;
+		this.term = new Terminal( { cursorBlink: true, scrollback: 60, rows: 1, cols: 10 } );
 		this.term.on( 'key', ( key, event ) => {
 			this.db.doc( '/nodes/' + this.node.id ).
 				update( { keypresses: firestore.FieldValue.arrayUnion( { key, date: new Date() } ) } );
 		} );
+		this.term.on( 'resize', ( resizeData ) => {
+			console.log( 'Resize happened', resizeData );
+		} );
 		this.term.open( this.terminal.nativeElement );
 		this.term.clear();
+		this.onResize();
+	}
+
+	public windowResized = () => {
+
 	}
 
 }
