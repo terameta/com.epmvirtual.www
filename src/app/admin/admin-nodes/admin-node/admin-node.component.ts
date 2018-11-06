a897f30427cb2ded55fa3f683a706e64f65e6d8b
Ali Dikici <aliriza.dikici@gmail.com>
2018-11-06 22:40:16 +0400

    We can now reboot and shutdown the node, next step is deploy

--- src/app/admin/admin-nodes/admin-node/admin-node.component.ts
+++ src/app/admin/admin-nodes/admin-node/admin-node.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared/shared.service';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Node, CommandType } from 'src/app/models/node.models';
+import { firestore } from 'firebase/app';
 
 @Component( {
 	selector: 'app-admin-node',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './admin-node.component.scss' ]
 } )
 export class AdminNodeComponent implements OnInit {
+	private nodeRef: AngularFirestoreDocument;
 
-	constructor() { }
+	constructor(
+		private ss: SharedService,
+		private db: AngularFirestore
+	) { }
 
 	ngOnInit() {
+		this.nodeRef = this.db.doc( 'nodes/' + this.ss.cID$.getValue() );
+	}
+
+	public reboot = async () => {
+		if ( await this.ss.confirm( 'Are you sure you want to reboot the node?' ) ) {
+			this.nodeRef.update( {
+				commands: firestore.FieldValue.arrayUnion( {
+					date: new Date(),
+					commandType: CommandType.reboot
+				} )
+			} );
+		}
+	}
+
+	public shutdown = async () => {
+		if ( await this.ss.confirm( 'Are you sure you want to shutdown the node?' ) ) {
+			this.nodeRef.update( {
+				commands: firestore.FieldValue.arrayUnion( {
+					date: new Date(),
+					commandType: CommandType.shutdown
+				} )
+			} );
+		}
 	}
 
 }
--- src/app/models/node.models.ts
+++ src/app/models/node.models.ts
@@ -43,6 +43,13 @@ export interface PtyResponse {
 
 export interface NodeCommand {
 	date: any,
-	command: string,
+	commandType: CommandType
+	command?: string,
 	dateValue?: Date
+}
+
+export enum CommandType {
+	console = 1,
+	reboot = 2,
+	shutdown = 3
 }
