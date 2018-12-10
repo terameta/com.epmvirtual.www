c760332ef3eca9688f938c9d6ca0876a4543adb8
Ali Dikici <aliriza.dikici@gmail.com>
2018-12-10 22:37:46 +0400

    Node details

--- src/app/admin/admin-nodes/admin-node-disk/admin-node-disk.component.ts
+++ src/app/admin/admin-nodes/admin-node-disk/admin-node-disk.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Node } from 'src/app/models/node.models';
+import { Observable } from 'rxjs';
+import { SharedService } from 'src/app/shared/shared.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { switchMap, map } from 'rxjs/operators';
+import { UtilitiesService } from 'src/app/shared/utilities.service';
 
-@Component({
-  selector: 'app-admin-node-disk',
-  templateUrl: './admin-node-disk.component.html',
-  styleUrls: ['./admin-node-disk.component.scss']
-})
+@Component( {
+	selector: 'app-admin-node-disk',
+	templateUrl: './admin-node-disk.component.html',
+	styleUrls: [ './admin-node-disk.component.scss' ]
+} )
 export class AdminNodeDiskComponent implements OnInit {
+	public codeToggle = false;
 
-  constructor() { }
+	public node$: Observable<Node> = this.ss.cID$.pipe(
+		switchMap( id => this.db.doc<Node>( 'nodes/' + id ).snapshotChanges() ),
+		map( a => this.us.action2Data<Node>( a ) )
+	);
 
-  ngOnInit() {
-  }
+	constructor(
+		public ss: SharedService,
+		private db: AngularFirestore,
+		private us: UtilitiesService
+	) { }
+
+	ngOnInit() {
+	}
 
 }
--- src/app/admin/admin-nodes/admin-node-memory/admin-node-memory.component.ts
+++ src/app/admin/admin-nodes/admin-node-memory/admin-node-memory.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Node } from 'src/app/models/node.models';
+import { switchMap, map } from 'rxjs/operators';
+import { SharedService } from 'src/app/shared/shared.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UtilitiesService } from 'src/app/shared/utilities.service';
 
-@Component({
-  selector: 'app-admin-node-memory',
-  templateUrl: './admin-node-memory.component.html',
-  styleUrls: ['./admin-node-memory.component.scss']
-})
+@Component( {
+	selector: 'app-admin-node-memory',
+	templateUrl: './admin-node-memory.component.html',
+	styleUrls: [ './admin-node-memory.component.scss' ]
+} )
 export class AdminNodeMemoryComponent implements OnInit {
+	public codeToggle = false;
 
-  constructor() { }
+	public node$: Observable<Node> = this.ss.cID$.pipe(
+		switchMap( id => this.db.doc<Node>( 'nodes/' + id ).snapshotChanges() ),
+		map( a => this.us.action2Data<Node>( a ) )
+	);
 
-  ngOnInit() {
-  }
+	constructor(
+		public ss: SharedService,
+		private db: AngularFirestore,
+		private us: UtilitiesService
+	) { }
+
+	ngOnInit() {
+	}
 
 }
