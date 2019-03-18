b87064635b75dd57986bd11ea60f0b7d7afba9c3
Ali Dikici <aliriza.dikici@gmail.com>
2019-03-18 23:36:21 +0400

    Preview is now enabled

--- src/app/admin/mail-templates/mail-template-detail/mail-template-detail.component.ts
+++ src/app/admin/mail-templates/mail-template-detail/mail-template-detail.component.ts
@@ -3,11 +3,12 @@ import { SharedService } from 'src/app/shared/shared.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { UtilitiesService } from 'src/app/shared/utilities.service';
 import { MailTemplate, MailTemplateType } from 'src/app/models/mailtemplate.models';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { enum2array } from 'src/utilities/utilityFunctions';
 import { Observable } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import { NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+import { systemSettings } from 'src/app/app.settings';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component( {
 	selector: 'app-mail-template-detail',
@@ -15,10 +16,18 @@ import { NgxMonacoEditorConfig } from 'ngx-monaco-editor';
 	styleUrls: [ './mail-template-detail.component.scss' ]
 } )
 export class MailTemplateDetailComponent implements OnInit {
+	public settings = systemSettings;
 	public types = enum2array( MailTemplateType );
+	public trustedUrl: SafeUrl = null;
 
 	public item$: Observable<MailTemplate> = this.ss.cItem$.pipe(
 		map( i => ( ( i as any ) as MailTemplate ) ),
+		tap( i => {
+			const u = 'https://us-central1-' + this.settings.firebase.projectId + '.cloudfunctions.net/previewMailTemplate?id=' + i.id + '&time=' + ( new Date() ).getTime();
+			// const u = '';
+			this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl( u );
+			console.log( this.trustedUrl );
+		} )
 	);
 
 	private editor: any;
@@ -43,7 +52,8 @@ export class MailTemplateDetailComponent implements OnInit {
 	constructor(
 		public ss: SharedService,
 		private db: AngularFirestore,
-		private us: UtilitiesService
+		private us: UtilitiesService,
+		private sanitizer: DomSanitizer
 	) { }
 
 	ngOnInit() { }
