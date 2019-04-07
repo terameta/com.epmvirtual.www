be8d2df72234358d6c927edf9a31a77c162e3f52
Ali Dikici <aliriza.dikici@gmail.com>
2019-04-07 23:15:45 +0400

    We now have a userprofile page

--- src/app/admin/profile/profile/profile.component.ts
+++ src/app/admin/profile/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component( {
 	selector: 'app-profile',
@@ -13,10 +14,21 @@ export class ProfileComponent implements OnInit {
 		private authService: AuthService
 	) { }
 
-	ngOnInit() {
-		this.authService.user.subscribe( user => {
-			console.log( user );
-			// user.email
+	ngOnInit() { }
+
+	public saveProfile = ( user: firebase.User, form: NgForm ) => {
+		const promises: Promise<any>[] = [];
+		console.log( form.form.value );
+		console.log( form.form.controls );
+		Object.entries( form.form.controls ).forEach( ( [ label, { dirty, ...control } ] ) => {
+			console.log( label, dirty, control );
+			if ( label === 'dName' && dirty ) promises.push( user.updateProfile( { displayName: control.value } ) );
+			if ( label === 'eMailAddress' && dirty ) promises.push( user.updateEmail( control.value ) );
+		} );
+		Promise.all( promises ).then( r => {
+			console.log( 'All Saved' );
+			console.log( r );
+			form.form.markAsPristine();
 		} );
 	}
 
